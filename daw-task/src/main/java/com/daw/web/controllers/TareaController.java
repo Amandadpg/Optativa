f3f751f4659ca368ed7b8bf39f99ecbf9f6d34dc
Adlpg <delapridamanda1@gmail.com>
2025-09-30 13:32:30 +0200

    cositas 2

--- daw-task/src/main/java/com/daw/web/controllers/TareaController.java
+++ daw-task/src/main/java/com/daw/web/controllers/TareaController.java
@@ -3,6 +3,8 @@ package com.daw.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,8 @@ public class TareaController {
 		return this.tareaService.findAll();
 	}
 	
-	public Tarea findById(int idTarea) {
+	@GetMapping("/idTarea") // 10
+	public Tarea findById(@PathVariable int idTarea) { 
 		return this.tareaService.findById(idTarea);
 	}
 	
