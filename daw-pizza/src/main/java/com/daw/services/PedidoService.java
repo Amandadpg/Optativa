dc91e1f5a595b9bc34030c36352a62f9adb20236
amandaDelaprida <aprigar802@g.educaand.es>
2025-11-11 18:25:56 +0100

    Terminado los create , update y deletes de todas las clases

--- daw-pizza/src/main/java/com/daw/services/PedidoService.java
+++ daw-pizza/src/main/java/com/daw/services/PedidoService.java
@@ -1,17 +1,18 @@
 package com.daw.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.daw.persistence.entities.Cliente;
+
 import com.daw.persistence.entities.Pedido;
+import com.daw.persistence.entities.Pizza;
+import com.daw.persistence.entities.enums.Metodo;
 import com.daw.persistence.repositories.PedidoRepository;
-import com.daw.services.exception.ClienteException;
+import com.daw.services.exception.PedidoException;
 import com.daw.services.exception.PedidoNotFoundException;
 import com.daw.services.exception.PizzaNotFoundException;
-import com.daw.persistence.entities.Pedido;
-import com.daw.persistence.repositories.PedidoRepository;
 
 @Service
 public class PedidoService {
@@ -33,12 +34,35 @@ public class PedidoService {
 	public Pedido create(Pedido pedido) {
 		
 		pedido.setId(0);
-		//mirar por si hay que poner algo mas
-		//hacer update y delete 
-		//y el pizza pedido entero
+		pedido.setFecha(LocalDate.now());
+		pedido.setTotal(0.0);
+		pedido.setMetodo(Metodo.LOCAL);
 		
 		
 		return this.pedidoRepository.save(pedido);
+	}
+	
+	public Pedido update(Pedido pedido, int idPedido) {
+		if (pedido.getId() != idPedido) {
+			throw new PedidoException("Los ids no coinciden");
+		}
+		if(!this.pedidoRepository.existsById(idPedido)) { 
+			throw new PedidoNotFoundException("EL pedido con id " + idPedido +" no existe");
+		}
+		
+		Pedido pedidoBD = this.findById(idPedido);
+		pedidoBD.setNotas(pedido.getNotas());
+		
+		
+		return this.pedidoRepository.save(pedidoBD);
+		
+	}
+	
+	public void delete(int idPedido) {
+		if(!this.pedidoRepository.existsById(idPedido)) {
+			throw new PedidoNotFoundException("EL pedido no existe.");
+		}
+		this.pedidoRepository.deleteById(idPedido);
 	}
 	
 }
--- daw-pizza/src/main/java/com/daw/services/PizzaPedidoService.java
+++ daw-pizza/src/main/java/com/daw/services/PizzaPedidoService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.daw.persistence.entities.PizzaPedido;
 import com.daw.persistence.repositories.PizzaPedidoRepository;
+import com.daw.services.exception.PizzaPedidoException;
+import com.daw.services.exception.PizzaPedidoNotFoundException;
 
 @Service
 public class PizzaPedidoService {
@@ -17,6 +19,44 @@ public class PizzaPedidoService {
 	public List<PizzaPedido> findAll() {
         return pizzaPedidoRepository.findAll(); 
     }
+	
+	public PizzaPedido findById(int idPizzaPedido) { 
+		if(!this.pizzaPedidoRepository.existsById(idPizzaPedido)) { 
+			throw new PizzaPedidoNotFoundException("La pizza del pedido con id " + idPizzaPedido +" no existe");
+		}
+		return this.pizzaPedidoRepository.findById(idPizzaPedido).get();
+	}
+	
+	public PizzaPedido create(PizzaPedido pizzaPedido) {
+		
+		pizzaPedido.setId(0);
+		pizzaPedido.setCantidad(0.0);
+		pizzaPedido.setPrecio(0.0);
+
+		
+		return this.pizzaPedidoRepository.save(pizzaPedido);
+	}
+	
+	public PizzaPedido update(PizzaPedido pizzaPedido, int idPizzaPedido) {
+		if (pizzaPedido.getId() != idPizzaPedido) {
+			throw new PizzaPedidoException("Los ids no coinciden");
+		}
+		if(!this.pizzaPedidoRepository.existsById(idPizzaPedido)) { 
+			throw new PizzaPedidoNotFoundException("La pizza del pedido con id " + idPizzaPedido +" no existe");
+		}
+		
+		PizzaPedido pizzaPedidoBD = this.findById(idPizzaPedido);
+		
+		return this.pizzaPedidoRepository.save(pizzaPedidoBD);
+		
+	}
+	
+	public void delete(int idPizzaPedido) {
+		if(!this.pizzaPedidoRepository.existsById(idPizzaPedido)) {
+			throw new PizzaPedidoNotFoundException("La pizza del pedido no existe.");
+		}
+		this.pizzaPedidoRepository.deleteById(idPizzaPedido);
+	}
 
 
 }
